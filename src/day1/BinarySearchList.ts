00189c9ddd650d19c59df30a1b524418cdcc0361
Thiago Verney <thiago.verney@gmail.com>
2024-09-01 12:04:12 -0400

    updated

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,18 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const midpoint = Math.floor(low + (high - low) / 2);
+        const value = haystack[midpoint];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = midpoint;
+        } else {
+            low = midpoint + 1;
+        }
+    } while (low < high);
+
+    return false;
+}
